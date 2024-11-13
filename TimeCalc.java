394f4845afd38d7fa921005679a5a3f39d4c5152
Arad Weinstein <arad.weinstein@post.runi.ac.il>
2024-11-13 16:36:39 +0200

    cleaning the code

--- TimeCalc.java
+++ TimeCalc.java
@@ -7,36 +7,15 @@
             // Parse minutesto add from the second argument
             int minutesToAdd = Integer.parseInt(args[1]);
             
-            // Convert to 12-hour format
-            //int hourFormat = hours > 12 ? (hours - 12) : hours;
-            //int hourFormat = hours;
-            
             // Calculate total minutes and update hours and minutes
             int totalMinutes = (hours * 60) + minutes + minutesToAdd;
             int totalHours = totalMinutes / 60;
             int newHours = totalHours % 24;
             int newMinutes = totalMinutes - (totalHours * 60);
-
-            // Convert to 12-hour format
-            //int newHourFormat = newHours == 12 ? (newHours - 12) : newHours;
-            int newHourFormat = newHours;
             
-            // Add padding of '0' to digits as needed 
-            String outputHour;
-            String outputMinute; 
-            if (newHourFormat < 10) {
-                outputHour = '0' + String.valueOf(newHourFormat);
-            }
-            else{
-                outputHour = String.valueOf(newHourFormat);
-            }
-
-            if (newMinutes < 10) {
-                outputMinute = '0' + String.valueOf(newMinutes);
-            }
-            else{
-                outputMinute = String.valueOf(newMinutes);
-            }
+            // Add padding of '0' to digits as needed
+            String outputHour = (newHours < 10 ? "0" : "") + newHours;
+            String outputMinute = (newMinutes < 10 ? "0" : "") + newMinutes;
 
             // Final output message
             System.out.println(outputHour + ':' + outputMinute);
