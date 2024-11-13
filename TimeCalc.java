e4653f52ba12c12becdc7e8927e3f826168a132c
Arad Weinstein <arad.weinstein@post.runi.ac.il>
2024-11-13 16:22:22 +0200

    fixing 12 hour format error

--- TimeCalc.java
+++ TimeCalc.java
@@ -1,27 +1,25 @@
     public class TimeCalc {
         public static void main(String[] args) {
-            // The following statement handles the hours part of the input.
-            // It concatenates the empty string "" with the leftmost hour-digit. 
-            // It then concatenates the resulting string with the rightmost hour-digit,
-            // and then uses parseInt to cast the resulting string as an int.
+            // Parse hours and minutes from the first argument in "hh:mm" format
             int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
-            // Does the same with the minutes part of the input.
             int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
             
+            // Parse minutesto add from the second argument
             int minutesToAdd = Integer.parseInt(args[1]);
             
             // Convert to 12-hour format
             //int hourFormat = hours > 12 ? (hours - 12) : hours;
-            int hourFormat = hours;
+            //int hourFormat = hours;
             
             // Calculate total minutes and update hours and minutes
-            int totalMinutes = (hourFormat * 60) + minutes + minutesToAdd;
+            int totalMinutes = (hours * 60) + minutes + minutesToAdd;
             int totalHours = totalMinutes / 60;
             int newHours = totalHours % 24;
             int newMinutes = totalMinutes - (totalHours * 60);
 
             // Convert to 12-hour format
-            int newHourFormat = newHours == 12 ? (newHours - 12) : newHours;
+            //int newHourFormat = newHours == 12 ? (newHours - 12) : newHours;
+            int newHourFormat = newHours;
             
             // Add padding of '0' to digits as needed 
             String outputHour;
